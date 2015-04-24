fba533d408ee39292e9c5f6f82527d1247e336e6
IGenerate <brent.stappaerts@gmail.com>
2015-04-24 14:30:32 +0200

    Facebook login aangemaakt

---
+++ configuration/config.js
@@ -0,0 +1,15 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+
+	"facebook_api_key" 		: 			"1438895059742545",
+	"facebook_api_secret"	:				"03b7487a124db3d3ceaef022664c2928",
+	"callback_url"				:				"http://localhost:3000/auth/facebook/callback",
+	"use_database"				:				"false",
+	"host"								:				"localhost",
+	"username"						:				"root",
+	"password"						:				"",
+	"database"						:				""
+}
